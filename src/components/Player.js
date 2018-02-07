cb54624ff190bafb97d7ee7f302923209a2c7f1d
xgirma <xgirma@gmail.com>
2018-02-06 23:34:01 -0800

    add player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import AudioPlayer from 'react-responsive-audio-player';
+import './Player.css';
+
+const playlist = [
+	{
+		url: 'http://benwiley4000.github.io/react-responsive-audio-player/audio/secret_of_trash_island.mp3',
+		displayText: 'Trash Island'
+	},
+	{
+		url: 'http://benwiley4000.github.io/react-responsive-audio-player/audio/marty_mcpaper_theme.mp3',
+		displayText: 'Marty McPaper'
+	},
+	{
+		url: 'http://benwiley4000.github.io/react-responsive-audio-player/audio/in_the_hall_of_the_mountain_king.mp3',
+		displayText: 'Mountain King'
+	}
+];
 
 class Player extends Component {
 	displayName = 'name';
@@ -11,13 +28,26 @@ class Player extends Component {
 	state = {};
 	
 	render() {
+		let audioElement = {};
 		return (
-			<div className="App-header">
-				<audio
-					// src="http://www.nihilus.net/soundtracks/Static%20Memories.mp3"
-					autoPlay>
-					Your browser does not support the <code>audio</code> element.
-				</audio>
+			<div className="App-Player">
+				<AudioPlayer
+					playlist={playlist}
+					controls={['spacer', 'backskip', 'playpause', 'forwardskip', 'spacer', 'progress']}
+					autoplay={true}
+					style={{position: 'fixed', top: 0}}
+					audioElementRef={function (elem) {
+						audioElement = elem;
+					}}
+					onMediaEvent={{
+						'playing': function (e) {
+							console.log('Playback has started', audioElement.src);
+						},
+						'pause': function (e) {
+							console.log('Playback was paused', audioElement.src);
+						}
+					}}
+				/>
 			</div>
 		)
 	}
