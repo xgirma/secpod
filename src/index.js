58c28d6ecf0bdc7f851beb748143b9d8fed58487
xgirma <xgirma@gmail.com>
2018-02-11 03:14:12 -0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Application from './components/Application';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Application />, document.getElementById('root'));
-registerServiceWorker();
